6d82348b117fb0588e35b17f25f0ad934b1ea502
kaalaa <catkbm25@gmail.com>
2025-10-01 22:26:36 +0530

    chore(deps): add preview library v0.3.0-rc1 and integrate @AllVariantsPreview
    - Added preview library v0.3.0-rc1 from Maven Central
    - Integrated @AllVariantsPreview annotation in MainActivity preview

--- app/src/main/java/com/karthik/pro/engr/architecting_state/MainActivity.kt
+++ app/src/main/java/com/karthik/pro/engr/architecting_state/MainActivity.kt
@@ -10,8 +10,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.karthik.pro.engr.architecting_state.ui.theme.ArchitectingstateTheme
+import com.karthik.pro.engr.devtools.AllVariantsPreview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +38,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@AllVariantsPreview
 @Composable
 fun GreetingPreview() {
     ArchitectingstateTheme {
