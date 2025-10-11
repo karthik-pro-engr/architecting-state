c3047d736f1c2995f65fb05534411a344aa8a218
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2025-10-11 23:17:44 +0530

    chore(build-logic): migrate build-logic module to dedicated repository
    - Removed in-repo build-logic module
    - Switched to consuming plugin version published from dedicated repository
    - Simplified project structure and centralized plugin maintenance

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 pluginManagement {
-    includeBuild("build-logic")
+
     repositories {
         google {
             content {
@@ -8,8 +8,26 @@ pluginManagement {
                 includeGroupByRegex("androidx.*")
             }
         }
+        maven {
+            url = uri("https://maven.pkg.github.com/karthik-pro-engr/build-logic")
+            credentials {
+                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
+                password = providers.gradleProperty("gpr.token").orNull ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            // Map plugin id -> exact published Maven coordinates (force the module Gradle should download)
+            if (requested.id.id == "karthik.pro.engr.android.application") {
+                useModule("karthik.pro.engr:android-application-plugin:${requested.version}")
+            }
+            if (requested.id.id == "karthik.pro.engr.android.library") {
+                useModule("karthik.pro.engr:android-library-plugin:${requested.version}")
+            }
+        }
     }
 }
 dependencyResolutionManagement {
